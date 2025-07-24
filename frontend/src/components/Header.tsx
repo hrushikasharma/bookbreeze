d7984ea9db3f9af58bd33f1464dbfcd80b816efa
Hrushika Sharma <hrushikaasharma04@gmail.com>
2025-07-24 12:02:38 +0530

    Initial commit - Bookbreeze project

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
-        <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to="/">MernHolidays.com</Link>
+        <span className="text-5xl text-yellow font-bold tracking-tight">
+          <Link to="/">BookBreeze</Link>
         </span>
         <span className="flex space-x-2">
           {isLoggedIn ? (
